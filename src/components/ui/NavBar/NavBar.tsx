55816df404588735eb1d1b197553eb9a6b90dc54
Youssef Tawfik <youssef.k.tawfik@outlook.com>
2025-02-25 08:39:00 +0200

    finished navbar

--- src/components/ui/NavBar/NavBar.tsx
+++ src/components/ui/NavBar/NavBar.tsx
@@ -1,3 +1,6 @@
+import CartIcon from "../../../assets/icons/CartIcon";
+import NavLink from "./components/NavLink";
+
 /**
  * NavBar component renders a navigation bar with links to different categories
  * of the website along with the logo and the shopping cart.
@@ -6,20 +9,17 @@ const NavBar: React.FC = (): JSX.Element => {
   return (
     <div className="container flex justify-between items-center">
       <nav>
-        <ul className="flex space-x-4">
-          <li>
-            <a href="/">All</a>
-          </li>
-          <li>
-            <a href="/clothes">Clothes</a>
-          </li>
-          <li>
-            <a href="/tech">Tech</a>
-          </li>
+        <ul className="flex">
+          <NavLink to="/">All</NavLink>
+          <NavLink to="/clothes">Clothes</NavLink>
+          <NavLink to="/tech">Tech</NavLink>
         </ul>
       </nav>
       <div>
         <img src="/logo.svg" alt="Logo" className="h-8" />
+      </div>
+      <div>
+        <CartIcon />
       </div>
     </div>
   );
---
+++ src/components/ui/NavBar/components/NavLink.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+import Style from "../NavBar.module.css";
+
+interface NavLinkProps {
+  to: string;
+  children: React.ReactNode;
+}
+
+const NavLink: React.FC<NavLinkProps> = ({ to, children }): JSX.Element => {
+  const location = useLocation();
+  const isActive = location.pathname === to;
+
+  return (
+    <li className={`${isActive ? Style.active : ""} p-4`}>
+      <a href={to}>{children}</a>
+    </li>
+  );
+};
+
+export default NavLink;
