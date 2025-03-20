d03743e1b40cb6b5b478e44582255364f574c331
Youssef Tawfik <youssef.k.tawfik@outlook.com>
2025-03-20 15:43:59 +0200

    preparing to add my orders tab

--- src/services/api/order.ts
+++ src/services/api/order.ts
@@ -12,6 +12,13 @@ interface PlaceOrderResponse {
   };
 }
 
+/**
+ * Places an order with the given order items.
+ *
+ * @param {OrderItem[]} orderItems - The items to be ordered.
+ * @returns {Promise<string>} - A promise that resolves to the order number.
+ * @throws {Error} - Throws an error if the order placement fails.
+ */
 const placeOrder = async (orderItems: OrderItem[]): Promise<string> => {
   const mutation = `
     mutation PlaceOrder($orderItems: [OrderItemInput!]!) {
@@ -31,7 +38,16 @@ const placeOrder = async (orderItems: OrderItem[]): Promise<string> => {
     throw new Error(response.data.errors[0].message);
   }
 
-  return response.data.data.placeOrder.order_number;
+  const orderNumber = response.data.data.placeOrder.order_number;
+
+  // append the order number to order_numbers in localStorage
+  const orderNumbers = JSON.parse(
+    localStorage.getItem("order_numbers") || "[]"
+  );
+  orderNumbers.push(orderNumber);
+  localStorage.setItem("order_numbers", JSON.stringify(orderNumbers));
+
+  return orderNumber;
 };
 
 export { placeOrder };
