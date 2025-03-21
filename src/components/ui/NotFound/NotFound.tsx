80ae31a421f8d56a9d9dc4eb55fa0256f51ca0dc
Youssef Tawfik <youssef.k.tawfik@outlook.com>
2025-03-22 01:37:16 +0200

    enhanced not found component

--- src/components/ui/NotFound/NotFound.tsx
+++ src/components/ui/NotFound/NotFound.tsx
@@ -1,5 +1,17 @@
+import React from "react";
+
+/**
+ * NotFound component displays a message indicating that the requested page was not found.
+ *
+ * @returns {JSX.Element} A JSX element containing the "Page Not Found" message and error code 404.
+ */
 const NotFound: React.FC = (): JSX.Element => {
-  return <div>Page Not Found. 404</div>;
+  return (
+    <div className="text-xl font-semibold text-center py-14">
+      <p>Page Not Found</p>
+      <p>ERROR: 404</p>
+    </div>
+  );
 };
 
 export default NotFound;
