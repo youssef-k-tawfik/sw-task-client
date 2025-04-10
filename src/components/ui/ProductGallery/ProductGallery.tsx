36a06577225d9d268af607ec90c1c11611fe55f4
Youssef Tawfik <youssef.k.tawfik@outlook.com>
2025-04-11 01:58:07 +0200

    refactored api url

--- src/components/ui/ProductGallery/ProductGallery.tsx
+++ src/components/ui/ProductGallery/ProductGallery.tsx
@@ -16,7 +16,7 @@ const ProductGallery: React.FC<ProductGalleryProps> = ({ images }) => {
 
   return (
     <div
-      className="w-full lg:w-2/3 pe-14 flex gap-2 justify-around"
+      className="w-full lg:w-2/3 pe-14 flex gap-2 justify-around h-[575px]"
       data-testid="product-gallery"
     >
       <div className="overflow-auto w-1/3 space-y-5">
@@ -31,7 +31,7 @@ const ProductGallery: React.FC<ProductGalleryProps> = ({ images }) => {
         ))}
       </div>
       {/* Main image */}
-      <div className="w-2/3 relative self-start h-[575px]">
+      <div className="w-2/3 relative self-start h-full">
         <img
           src={selectedImage}
           alt="product image"
--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL + "/graphql",
   headers: {
     "Content-Type": "application/json",
   },
